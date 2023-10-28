3ce6cc70c8d3fba7fa65cb1cfdc567bfe71c6538
Bárbara <babivaldez@gmail.com>
2023-10-28 12:57:33 -0300

    corrige erro de lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,7 @@
 # se eu não colocar o return no primeiro if, eu não consigo capturar o true
 # da linha 6, pois o resultado True retorna para a chamada da função
-# imediatamente anterior, mas essa informação não é propagada para as outras chamadas recursivas.
+# imediatamente anterior, mas essa informação não é
+# propagada para as outras chamadas recursivas.
 
 
 def is_palindrome_recursive(word, low_index, high_index):
