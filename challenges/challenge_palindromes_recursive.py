f3d022b2648df4fda91ae415cc06e319527f8bfd
Bárbara <babivaldez@gmail.com>
2023-10-28 13:02:57 -0300

    acrescente um if na função para verificar se nenhuma palavra foi passada por parâmetro

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -5,6 +5,8 @@
 
 
 def is_palindrome_recursive(word, low_index, high_index):
+    if len(word) == 0:
+        return False
     if low_index <= high_index and word[low_index] == word[high_index]:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     if low_index > high_index:
