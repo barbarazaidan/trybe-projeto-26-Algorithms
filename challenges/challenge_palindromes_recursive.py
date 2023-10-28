06734898ab1ad48d2b3544089097ea8a4dd8a282
Bárbara <babivaldez@gmail.com>
2023-10-28 12:53:21 -0300

    cria a função is_palindrome_recursive - req3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
+# se eu não colocar o return no primeiro if, eu não consigo capturar o true
+# da linha 6, pois o resultado True retorna para a chamada da função
+# imediatamente anterior, mas essa informação não é propagada para as outras chamadas recursivas.
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if low_index <= high_index and word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    if low_index > high_index:
+        return True
+    return False
