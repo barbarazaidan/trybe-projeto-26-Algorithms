295b66099741d84ddac80009afa7899e210d9f72
Bárbara <babivaldez@gmail.com>
2023-10-29 17:34:02 -0300

    acrescenta a validação para target_time ser valor vazio e altera a linha 9

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,17 +1,15 @@
 def study_schedule(permanence_period, target_time):
     count = 0
     n = len(permanence_period)
+    if target_time is None:
+        return None
     for index in range(n):
         start_time = permanence_period[index][0]
         end_time = permanence_period[index][1]
-        if (
-            start_time == target_time
-            or end_time == target_time
-            or start_time < target_time < end_time
-        ):
+        if start_time <= target_time <= end_time:
             count += 1
     return count
 
 
-# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-# print(study_schedule(permanence_period, 2))
+permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+print(study_schedule(permanence_period, None))
