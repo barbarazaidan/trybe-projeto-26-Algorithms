9a548a1d6467f1a1d63e8f80c64b93eda92dae11
Bárbara <babivaldez@gmail.com>
2023-10-29 17:41:49 -0300

    acrescente validação para tuplas vazias ou valores não numéricos

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -6,10 +6,14 @@ def study_schedule(permanence_period, target_time):
     for index in range(n):
         start_time = permanence_period[index][0]
         end_time = permanence_period[index][1]
+        if start_time is None or end_time is None:
+            return None
+        if not isinstance(start_time, int) or not isinstance(end_time, int):
+            return None
         if start_time <= target_time <= end_time:
             count += 1
     return count
 
 
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-print(study_schedule(permanence_period, None))
+# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# print(study_schedule(permanence_period, None))
