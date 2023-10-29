abf6bd66d6c775d8ffc43dfe76e8dea74ee4e575
Bárbara <babivaldez@gmail.com>
2023-10-29 14:09:47 -0300

    corrige erro do lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,15 +2,16 @@ def study_schedule(permanence_period, target_time):
     count = 0
     n = len(permanence_period)
     for index in range(n):
+        start_time = permanence_period[index][0]
+        end_time = permanence_period[index][1]
         if (
-            permanence_period[index][0] == target_time
-            or permanence_period[index][1] == target_time
+            start_time == target_time
+            or end_time == target_time
+            or start_time < target_time < end_time
         ):
-            count += 1
-        if permanence_period[index][0] < target_time < permanence_period[index][1]:
             count += 1
     return count
 
 
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-print(study_schedule(permanence_period, 2))
+# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# print(study_schedule(permanence_period, 2))
