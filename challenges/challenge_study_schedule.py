5823487a35703fd54f6adc46a10b773d9aa0b804
Bárbara <babivaldez@gmail.com>
2023-10-29 13:14:28 -0300

    inicia a criação da função study_schedule - req.01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    n = len(permanence_period)
+    for index in range(n):
+        if (
+            permanence_period[index][0] == target_time
+            or permanence_period[index][1] == target_time
+        ):
+            count += 1
+        if permanence_period[index][0] < target_time < permanence_period[index][1]:
+            count += 1
+    return count
+
+
+permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+print(study_schedule(permanence_period, 2))
